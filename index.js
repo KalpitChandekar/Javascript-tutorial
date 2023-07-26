3517b4d11c3d7f8aadbab363c39718e189614309
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-07-26 20:45:38 +0530

    create lovescore generator

---
+++ index.js
@@ -0,0 +1,10 @@
+//--Pseudo random number generator--//
+
+prompt("What is your name ?");
+prompt("What is their name ?");
+
+var loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+alert("Your loveScore is " + loveScore);
