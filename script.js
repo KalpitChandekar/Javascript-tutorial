65bb802f80cc32fc2ba641904f92cdc4567f1075
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-07-28 13:58:10 +0530

    completed fizzbuzz challenge

--- script.js
+++ script.js
@@ -28,12 +28,35 @@
 
 //--------- array-----
 
-var guestList = ['kalpit','karan','amol','yash','parish','samir','manish','sanu'];
+// var guestList = ['kalpit','karan','amol','yash','parish','samir','manish','sanu'];
 
-var guestName = prompt("Enter your name");
+// var guestName = prompt("Enter your name");
 
-if (guestList.includes(guestName)) {
-  alert('Welcome');
-}else{
-  alert('Sorry, maybe be next time')
+// if (guestList.includes(guestName)) {
+//   alert('Welcome');
+// }else{
+//   alert('Sorry, maybe be next time')
+// }
+
+// -----fIZZBUZZ ---//
+
+var output = [];
+
+var count = 1;
+
+function fizzBuzz() {
+
+  if (count % 3 ===0 && count % 5 === 0) {
+    output.push("FizzBuzz");
+  } else if (count % 3 === 0) {
+    output.push("Fizz");
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+
+  count++;
+
+  console.log(output);
 }
